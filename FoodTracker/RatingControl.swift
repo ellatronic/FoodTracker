6f268353244d60a8c7b8d93218cc76742b33b79b
ellatronic <ellachiangg@gmail.com>
2017-01-30 08:41:48 -0800

    Set up initialization.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -11,6 +11,10 @@ import UIKit
 class RatingControl: UIStackView {
     //MARK: Initialization
     override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
         <#code#>
     }
 }
